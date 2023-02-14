398cbd240ee2ab03d115352bac727072774faa6f
Ryan <wrosullivan88@gmail.com>
2023-02-14 16:42:15 -0600

    working on validations...

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,20 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :burger
+
+  validates :name, presence: :true
+  validates :city, presence: :true
+  validates :state_abbr, presence: :true, length: {is: 2}
+  validates :zip, length: {is: 5}
+
+
+
+
+
+
+
+
+
+  #
 end
 
-
-#burger has one rest
