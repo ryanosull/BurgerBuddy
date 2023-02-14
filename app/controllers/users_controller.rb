f3e5a0b20926c4ccb5ea9464e8de4088bcfde4b7
Ryan <wrosullivan88@gmail.com>
2023-02-13 21:57:06 -0600

    FUCK YOU LATE NIGHT COMMITS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
+    skip_before_action :authorized_user, only: [:create]
+
     def show
-        user = User.find(params[:id])
-        render json: user, status: :ok
+        # user = User.find(session[:user_id])
+        # user = User.find(params[:id])
+        render json: current_user, status: :ok
     end
 
     def create
         user = User.create!(user_params)
+        session[:user_id] = user.id
         render json: user, status: :created
     end
 
