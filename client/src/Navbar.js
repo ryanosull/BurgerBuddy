7e96682c628e531dbf207ba7af5f5951ccfa4c38
Ryan <wrosullivan88@gmail.com>
2023-02-14 19:59:49 -0600

    starting google fonts, html tags added

---
+++ client/src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+function Navbar () {
+
+
+
+    // function handleLogOut () {
+    //     fetch('/logout', {
+    //         method: 'DELETE'
+    //     })
+    // }
+
+
+
+
+
+    return (
+        <div>
+            navigation
+        </div>
+    );
+};
+
+export default Navbar;
