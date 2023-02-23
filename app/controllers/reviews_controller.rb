ed60eb2fcbf9600ba65ee2de002be4e873d2cf0f
William Ryan OSullivan <wrosullivan@gmail.com>
2023-02-23 11:32:36 -0600

    almost done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,34 @@
 class ReviewsController < ApplicationController
+
+
+    def index 
+        reviews = Review.all
+        render json: reviews, except: [:created_at, :updated_at], status: :ok
+    end
+
+    def show 
+        review = Review.find_by(id: params[:id])
+        if review
+            render json: review.to_json(except: [:created_at, :updated_at]), status: :ok
+        else
+            review_not_found
+        end
+    end
+
+    def create 
+        
+    end
+
+
+
+
+    private
+
+    def review_not_found
+        render json: {errors: ['If no news is good news, is no reviews good reviews?']}
+    end
+
+
+
+    #
 end
