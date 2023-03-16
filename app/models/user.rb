7a0b132a2a53205c22cc06c0d5d9cdd40df4ff47
William Ryan OSullivan <wrosullivan@gmail.com>
2023-03-16 17:17:38 -0500

    30 min into pt 1 pdawg

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, presence: true, length: {minimum: 5, maximum:20}
 
 
