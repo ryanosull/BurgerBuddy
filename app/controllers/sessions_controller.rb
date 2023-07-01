5cafc2ef7d49f3c3b0aaafbbd8a8d4e1fb623b91
William Ryan OSullivan <wrosullivan@gmail.com>
2023-06-30 23:30:43 -0500

    sessions controller create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,23 @@ class SessionsController < ApplicationController
     skip_before_action :authorized_user, only: [:create] 
     #keep this one ^
 
-    def create
-        user = User.find_by(email: params[:email])
-        if user&.authenticate(params[:password])
-            session[:user_id] = user.id
-            render json: user, status: :created
+    # def create
+    #     user = User.find_by(email: params[:email])
+    #     if user&.authenticate(params[:password])
+    #         session[:user_id] = user.id
+    #         render json: user, status: :created
+    #     else
+    #         render json: {errors: ["Incorrect email or password!"]}, status: :unauthorized
+    #     end
+    # end
+
+    def create 
+        @user = User.find_by(email: params[:email])
+        if @user and @user.authenticate(params[:password])
+            logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN'])
+            render json: {uid: logged_user}, status: :ok
         else
-            render json: {errors: ["Incorrect email or password!"]}, status: :unauthorized
+            render json: {errors: ["Incorrect email and/or password!"]}, status: :unauthorized
         end
     end
 
