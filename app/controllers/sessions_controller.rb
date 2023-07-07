19fb618c1df1cc2c4fd9c1fc76e18d072e11444a
William Ryan OSullivan <wrosullivan@gmail.com>
2023-07-06 20:26:17 -0500

    fuck you

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,37 +1,36 @@
 class SessionsController < ApplicationController
 
-
-    # def create
-    #     user = User.find_by(email: params[:email])
-    #     if user&.authenticate(params[:password])
-    #         session[:user_id] = user.id
-    #         render json: user, status: :created
+    # below should be good, redoing to follow video
+    # def create 
+    #     @user = User.find_by(email: params[:email])
+    #     if @user and @user.authenticate(params[:password])
+    #         logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN']) # 'user' for app_controller/#current_user
+    #         user = {id: @user.id, uid: logged_user}
+    #         render json: {uid: logged_user}, status: :ok #200
     #     else
-    #         render json: {errors: ["Incorrect email or password!"]}, status: :unauthorized
+    #         cannot_login
     #     end
     # end
 
     def create 
         @user = User.find_by(email: params[:email])
         if @user and @user.authenticate(params[:password])
-            logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN']) # 'user' for app_controller/#current_user
-            # user = {id: @user.id, uid: logged_user}
+            logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN'])
             render json: {uid: logged_user}, status: :ok #200
-        else
-            cannot_login
         end
     end
+    
 
-    def auto_login
-        auth_token = request.headers['auth-token'] #do not use underscore @ 'auth-token'
-        if auth_token and auth_token != 'undefined'
-            token = JWT.decode(auth_token, ENV['JWT_TOKEN'])[0] #pull out array
-            user = User.find_by( id: token['user']) #'user' from sessions_controller/#create
-            render json: user.id, status: :ok #200
-        else
-            cannot_login
-        end
-    end
+    # def auto_login
+    #     auth_token = request.headers['auth-token'] #do not use underscore @ 'auth-token'
+    #     if auth_token and auth_token != 'undefined'
+    #         token = JWT.decode(auth_token, ENV['JWT_TOKEN'])[0] #pull out array
+    #         user = User.find_by( id: token['user']) #'user' from sessions_controller/#create
+    #         render json: user.id, status: :ok #200
+    #     else
+    #         cannot_login
+    #     end
+    # end
 
     # def destroy
     #     session.delete :user_id
