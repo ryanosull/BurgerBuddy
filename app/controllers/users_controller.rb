88897f062642c37385d3e8bd650de9745fc4e006
William Ryan OSullivan <wrosullivan@gmail.com>
2023-07-15 22:27:15 -0500

    ended up mostly in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,37 +5,32 @@ class UsersController < ApplicationController
     # skip_before_action :authorize!, only: [:create] - probably ok
     wrap_parameters format: [] #should this belong in ApplicationController?
 
-    # def index
-    #     users = User.all
-    #     render json: users, status: :ok
-    # end
-
-    def show
-        current_user = User.find(params[:id]) #(session[:user_id]) 7/20
-        user = User.find(params[:id])
-        render json: current_user, status: :ok #user
+    def index     #7/20 determining if necessary - leaving on for Postman
+        users = User.all
+        render json: users, status: :ok
     end
 
-    # def show
-    #     user = User.find_by(id: params[:id])
-    #     if user
-    #         render json: user.to_json(except: [:created_at, :updated_at, :password_digest]), status: :ok
-    #     else
-    #         user_not_found
-    #     end
-    # end
+    def show #7/20 maybe come back and restrict pw_dig
+        user = User.find_by(id: params[:id]) #(session[:user_id]) 7/20
+        if user
+            render json: user, status: :ok #user
+        else
+            user_not_found #7/20 working as intended
+        end
+    end
 
-    def create ###good to go
+
+    def create # 7/20 good to go
         user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created #201
     end
 
-    def update
+    def update # 7/20 good to go with auth-token 
         user = User.find_by(id: params[:id])
         if user
             user.update(user_params)
-            render json: user
+            render json: user, status: :ok #200
         else
             render json: {errors: ["User does not exist!"]} #should render authorize!
         end
@@ -55,7 +50,7 @@ class UsersController < ApplicationController
     private
     
     def user_params #come back!
-        params.permit(:first_name, :last_name, :email, :password, :password_confirmation) #do not use :password_digest for :password
+        params.permit(:first_name, :last_name, :email, :password, :password_confirmation) #do not use :password_digest for :password 7/20
     end
 
     #why not this?:
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :password #password_confirmation not necessary here??
+  attributes :id, :first_name, :last_name, :email, :password_digest #7/20 good - still unknown of pw_conf necessary at this pont
 
   has_many :reviews
   has_many :burgers
