dcdcc6ec82531fbe6e369ac0818bec8481523e76
William Ryan OSullivan <wrosullivan@gmail.com>
2023-07-16 04:38:42 -0500

    NO ERRORS AT THE MOMENT

--- client/src/Reviews.js
+++ client/src/Reviews.js
@@ -1,65 +1,65 @@
-// import React from "react";
-// import {useState, useEffect} from "react"
-// import ReviewCard from "./ReviewCard"
-// import "./Reviews.css"
+import React from "react";
+import {useState, useEffect} from "react"
+import ReviewCard from "./ReviewCard"
+import "./Reviews.css"
 
 
 
-// function Reviews (args) {
+function Reviews (args) {
 
-//     const [reviews, setReviews] = useState([])
+    const [reviews, setReviews] = useState([])
 
-//     // useEffect(() => fetchReviews(), [])
+    // useEffect(() => fetchReviews(), [])
 
-//     // function fetchReviews () {
-//     //     fetch("/reviews")
-//     //     .then(r => r.json())
-//     //     .then(setReviews)
-//     // }
-
-
-
-//     const [user, setUser] = useState(null) // user state initialized as null
+    // function fetchReviews () {
+    //     fetch("/reviews")
+    //     .then(r => r.json())
+    //     .then(setReviews)
+    // }
 
 
-//     useEffect( () => {
-//     fetch(`/users/${args.currentUser}`,	{
-//         headers: {
-//         'Content-Type': 'application/json',
-//         'auth-token': localStorage.uid
-//         } 
-//     } )
-//     .then(resp => resp.json())
-//     .then(data => console.log(data))
-//     }, []);
+
+    const [user, setUser] = useState(null) // user state initialized as null
 
 
-//     const modifiedArray = (id) => setReviews(current => current.filter(review => review.id !== id)) 
-
-//     function handleDelete(){
-//         setReviews(modifiedArray) //setCurrentUser
-//     }
-
-
-//     const renderReviewCard = reviews.filter(review => review.user.id === args.currentUser.id).map (review =>
-//         <ReviewCard
-//         review={review}
-//         key={review.id}
-//         handleDelete={handleDelete}
-//         modifiedArray={modifiedArray}
-//         setReviews={setReviews}
-//         />
-//         )
+    useEffect( () => {
+    fetch(`/users/${args.currentUser}`,	{
+        headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.uid
+        } 
+    } )
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    }, []);
 
 
-//     return (
+    const modifiedArray = (id) => setReviews(current => current.filter(review => review.id !== id)) 
 
-//         <div id="reviewContainer" >
-//             {user.reviews.length > 0 ? renderReviewCard : <h1 id="noBurger">Go get yourself a burger, {user.first_name}.</h1>}
-//         </div>
+    function handleDelete(){
+        setReviews(modifiedArray) //setCurrentUser
+    }
+
+
+    const renderReviewCard = reviews.filter(review => review.user.id === args.currentUser.id).map (review =>
+        <ReviewCard
+        review={review}
+        key={review.id}
+        handleDelete={handleDelete}
+        modifiedArray={modifiedArray}
+        setReviews={setReviews}
+        />
+        )
+
+
+    return (
+
+        <div id="reviewContainer" >
+            {user.reviews.length > 0 ? renderReviewCard : <h1 id="noBurger">Go get yourself a burger, {user.first_name}.</h1>}
+        </div>
     
-//     );
-// };
+    );
+};
 
 
-// export default Reviews;
+export default Reviews;
